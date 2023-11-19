663796be89bda996e7f083f9042fe106352ffd88
amitsinghtandon <108742253+amitsinghtandon@users.noreply.github.com>
2023-11-19 13:05:48 +0530

    images location

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let randomNumber1 = Math.floor(Math.random()*6+1);
 
-let randomImage = "./images/dice"+ randomNumber1 + ".png";
+let randomImage = "../images/dice"+ randomNumber1 + ".png";
 
 let image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src",randomImage);
 
 let randomNumber2 = Math.floor(Math.random()*6+1);
-let randomImage2 = "./images/dice"+ randomNumber2 + ".png";
+let randomImage2 = "../images/dice"+ randomNumber2 + ".png";
 
 let image2 = document.querySelectorAll("img")[1];
 
